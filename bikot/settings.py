a24a358bb6dd5704bb8a6618c498f94938241d2c
braynerdev <dev.brayner@outlook.com>
2026-06-05 19:32:15 -0300

    feat: create superuser

--- bikot/settings.py
+++ bikot/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 
 import os
 
@@ -188,10 +191,18 @@ STORAGES = {
 
 @receiver(post_migrate)
 def create_superuser(sender, **kwargs):
-    from django.contrib.auth.models import User
-    if User.objects.count() == 0:
+    User = get_user_model()
+
+    username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+    email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+    password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+    if not all([username, email, password]):
+        return
+
+    if not User.objects.filter(username=username).exists():
         User.objects.create_superuser(
-            username=os.getenv("DJANGO_SUPERUSER_USERNAME"),
-            email=os.getenv("DJANGO_SUPERUSER_EMAIL"),
-            password=os.getenv("DJANGO_SUPERUSER_PASSWORD")
+            username=username,
+            email=email,
+            password=password,
         )
